2bc64bd5bb5265c067edb73e7911d759969ec9b7
vircag <vircagoncalves39@gmail.com>
2020-11-13 15:33:21 +0000

    criação da atividade login

--- app/src/main/java/com/example/healthyeconomy/LoginActivity.java
+++ app/src/main/java/com/example/healthyeconomy/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.healthyeconomy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void abrirInserirGastos( View view){
+        Intent intent = new Intent(LoginActivity.this,InserirGastosPropiosActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void abrirRegistarUtilizador( View view){
+        Intent intent = new Intent(LoginActivity.this,ResgistarActivity2.class);
+        startActivity(intent);
+
+    }
+
 }
