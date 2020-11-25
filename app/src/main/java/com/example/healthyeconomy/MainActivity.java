7c29a79c4f6cb4ecaa16d63859364ae27d6f519f
vircag <vircagoncalves39@gmail.com>
2020-11-25 14:57:02 +0000

    Criação da atividade principal

--- app/src/main/java/com/example/healthyeconomy/MainActivity.java
+++ app/src/main/java/com/example/healthyeconomy/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+        /*
+        Utilizador utilizador = new Utilizador();
+        utilizador.setNome("Virca");
+        utilizador.setSobrenome("Afonso");
+        utilizador.setEmail("vircag@outlook.pt");
+        utilizador.setEstadoCivil("solteira");
+        utilizador.setFilhos(false);
+        utilizador.setIdade(24);
+        utilizador.setMorada("Rua miguel torga 2º direito");
+        utilizador.setNumTelefone("927763881");
+*/
     }
 }
