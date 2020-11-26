33fa13e6584aff2bdc22a8a9ce2a0555c61a521e
vircag <vircagoncalves39@gmail.com>
2020-11-26 15:53:41 +0000

    adição do firebase na atividade login

--- app/src/main/java/com/example/healthyeconomy/LoginActivity.java
+++ app/src/main/java/com/example/healthyeconomy/LoginActivity.java
@@ -6,23 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class LoginActivity extends AppCompatActivity {
+    private DatabaseReference referenciaFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        referenciaFirebase = ConfiguracaoFirebase.getFirebase();
+        referenciaFirebase.child("pontos").setValue("800");
+        referenciaFirebase.child("pontos1").setValue("8000");
     }
-    public void abrirInserirGastos( View view){
+    public void abrirInserirGastos(View view){
         Intent intent = new Intent(LoginActivity.this,InserirGastosPropiosActivity.class);
         startActivity(intent);
 
     }
 
-    public void abrirRegistarUtilizador( View view){
-        Intent intent = new Intent(LoginActivity.this,ResgistarActivity2.class);
+     public void abrirRegistarUtilizador(View view){
+        Intent intent = new Intent(LoginActivity.this,RegistarUtilizadorActivity.class);
         startActivity(intent);
 
-    }
-
+     }
 }
