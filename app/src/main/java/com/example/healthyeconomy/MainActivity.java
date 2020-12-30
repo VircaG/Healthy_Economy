a0081c1b1a7eec460d1bbd107dc8b5180c02e3b1
VircaG <vircagoncalves39@gmail.com>
2020-12-30 18:02:35 +0000

    configuracao para validar o registo do utilizador

--- app/src/main/java/com/example/healthyeconomy/MainActivity.java
+++ app/src/main/java/com/example/healthyeconomy/MainActivity.java
@@ -1,20 +1,43 @@
 package com.example.healthyeconomy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Firebase firebase;
+    private Button botaoSair;
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Healthy Economy");
+       // setSupportActionBar(toolbar);
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main);
+        return true;
+    }
+    */
+
 }
