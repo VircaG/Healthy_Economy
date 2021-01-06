415487208b10199f1d2a32557f6112f6a26ac48d
VircaG <vircagoncalves39@gmail.com>
2021-01-06 17:18:29 +0000

    Função para Logout

--- app/src/main/java/com/example/healthyeconomy/MainActivity.java
+++ app/src/main/java/com/example/healthyeconomy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.healthyeconomy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,9 +18,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private Firebase firebase;
-    private Button botaoSair;
     private Toolbar toolbar;
+    private FirebaseAuth utilizadorAutenticacao;
 
 
 
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        utilizadorAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Healthy Economy");
@@ -39,5 +42,28 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()){
+        case R.id.item_sair:
+            logoutUtilizador();
+            return true;
+        case R.id.item_pesquisa:
+            return true;
+        case R.id.item_contacto:
+            return true;
+        case R.id.item_configuracoes:
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
 
+         }
+    }
+    public void logoutUtilizador(){
+        utilizadorAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
 }
