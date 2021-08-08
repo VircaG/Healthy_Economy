db588c3aba9e9b1d2247c6ba6cf4caa08f6788e0
Virca Gonçalves <vircagoncalves39@gmail.com>
2021-08-08 23:26:22 +0100

    Alterações no modelo Limite mensal

--- app/src/main/java/com/example/healthyeconomy/LimiteMensal.java
+++ app/src/main/java/com/example/healthyeconomy/LimiteMensal.java
@@ -9,34 +9,20 @@ import com.google.firebase.database.DatabaseReference;
 
 public class LimiteMensal {
 
-    private String idLimiteMensal;
+    private String idUtilizador;
     private String valorLimite;
-    private Button inserirDespesas;
-    private Button salvarLimite;
 
 
 
-    public LimiteMensal(){
-        DatabaseReference reference = ConfiguracaoFirebase.getFirebase().child("limite mensal");
-        setIdLimiteMensal(reference.push().getKey());
-    }
-    public void guardarLimite(){
-        FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-        String idUtilizador = Base64Custom.condificarBase64(autenticacao.getCurrentUser().getEmail());
-
-        DatabaseReference firebase = ConfiguracaoFirebase.getFirebase();
-            firebase.child("Limite Mensal")
-                    .child(idUtilizador)
-                    .child(idLimiteMensal)
-                    .setValue(this);
+    public LimiteMensal() {
     }
 
-    public String getIdLimiteMensal() {
-        return idLimiteMensal;
+    public String getIdUtilizador() {
+        return idUtilizador;
     }
 
-    public void setIdLimiteMensal(String idLimiteMensal) {
-        this.idLimiteMensal = idLimiteMensal;
+    public void setIdUtilizador(String idUtilizador) {
+        this.idUtilizador = idUtilizador;
     }
 
     public String getValorLimite() {
@@ -45,21 +31,5 @@ public class LimiteMensal {
 
     public void setValorLimite(String valorLimite) {
         this.valorLimite = valorLimite;
-    }
-
-    public Button getInserirDespesas() {
-        return inserirDespesas;
-    }
-
-    public void setInserirDespesas(Button inserirDespesas) {
-        this.inserirDespesas = inserirDespesas;
-    }
-
-    public Button getSalvarLimite() {
-        return salvarLimite;
-    }
-
-    public void setSalvarLimite(Button salvarLimite) {
-        this.salvarLimite = salvarLimite;
     }
 }
