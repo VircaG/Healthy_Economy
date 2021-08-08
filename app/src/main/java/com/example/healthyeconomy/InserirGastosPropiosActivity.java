6b2feb011855a2d1fd43760c73534a0bad4adccc
Virca Gonçalves <vircagoncalves39@gmail.com>
2021-08-08 23:30:23 +0100

    Carregar dados da tabela gastos proprios para firebase

--- app/src/main/java/com/example/healthyeconomy/InserirGastosPropiosActivity.java
+++ app/src/main/java/com/example/healthyeconomy/InserirGastosPropiosActivity.java
@@ -19,58 +19,72 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 
-public class InserirGastosPropiosActivity extends AppCompatActivity implements  AdapterView.OnItemSelectedListener { private static final String TAG ="InserirGastosPropiosActivity";
-  private TextView  mDisplayDate;
-  private DatePickerDialog.OnDateSetListener mDateSetListener;
-  private String id;
-  private EditText descricao;
-  private EditText valor;
-  private TextView data;
-  Spinner spinerCategorias;
-  private GastosPropios gastosPropios ;
-  private Button botaoInserir;
-  private Button botaoVisualizar;
+public class InserirGastosPropiosActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private FirebaseAuth utilizadorFirebase;
+    private DatabaseReference firebase;
 
-  //Spinner customSpinner;
-  //ArrayList<CustomItem> customList;
-  //FirebaseDatabase database;
-  //DatabaseReference reference;
-  int maxid = 0;
+    //private String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+    private static final String TAG ="InserirGastosPropiosActivity";
+    private TextView  mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private EditText descricao;
+    private EditText valor;
+    private TextView data;
+    Spinner spinerCategorias;
+    private Button botaoInserir;
+    private Button botaoVisualizar;
+
+
+    private  String  idUtilizadorGP;
+
+    //Spinner customSpinner;
+    //ArrayList<CustomItem> customList;
+    //private String identificadorUtilizador;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir_gastos_propios);
 
-        mDisplayDate = (TextView) findViewById(R.id.tv_data_gasto);
-        spinerCategorias = (Spinner) findViewById(R.id.spiner_Categorias);
+        utilizadorFirebase = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
-        //customSpinner = findViewById(R.id.customIconSpinner);
-        //customList = getCustomList();
-       // CostimizacaoAdapter adapter1 = new CostimizacaoAdapter(this,customList);
-       // customSpinner.setAdapter(adapter1);
-       // customSpinner.setOnItemSelectedListener(this);
-        //outros itenscustomIconSpinner
-       // customSpinner = findViewById(R.id.customIconSpinner);
+
+        //userFirebase = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+
+        mDisplayDate = (TextView) findViewById(R.id.tv_data_gasto);
+
 
 
         descricao = (EditText)findViewById(R.id.edit_regitar_decricao);
-          valor =    (EditText)  findViewById(R.id.edit_registar_valor);
-          data =     (TextView)    findViewById(R.id.tv_data_gasto);
+        valor =    (EditText)  findViewById(R.id.edit_registar_valor);
+        data =     (TextView)    findViewById(R.id.tv_data_gasto);
+        spinerCategorias = (Spinner) findViewById(R.id.spiner_Categorias);
+
         botaoInserir = (Button) findViewById(R.id.btn_inserirGastos);
         botaoVisualizar = (Button) findViewById(R.id.btn_visualizarGastos);
-        gastosPropios = new GastosPropios();
-//        reference = database.getInstance().getReference().child("Gastos Proprios");
 
-       mDisplayDate .setOnClickListener(new View.OnClickListener() {
+        //Dados do Utilizador Logado
+        Preferencias preferencias = new Preferencias(InserirGastosPropiosActivity.this);
+        idUtilizadorGP = preferencias.getIdentificador();
+
+
+        mDisplayDate .setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar cal = Calendar.getInstance();
@@ -95,7 +109,7 @@ public class InserirGastosPropiosActivity extends AppCompatActivity implements  
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 month = month +1;
                 Log.d("InserirGastosPropiosActivity","onDateSet: mm/dd/yyy:" + month
-               + "/" + day + "/" + year);
+                        + "/" + day + "/" + year);
 
                 String date = month + "/" + day +"/"+ year;
                 mDisplayDate.setText(date);
@@ -104,58 +118,52 @@ public class InserirGastosPropiosActivity extends AppCompatActivity implements  
         };
 
 
-//        reference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                if(snapshot.exists()){
-//                    maxid =(int) snapshot.getChildrenCount();
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+
         botaoInserir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gastosPropios = new GastosPropios();
-                //gastosPropios.setSpinner(customSpinner.getSelectedItem().toString());
-                gastosPropios.setDecricao(descricao.getText().toString());
-                gastosPropios.setValor(valor.getText().toString());
-                gastosPropios.setData(data.getText().toString());
-                gastosPropios.setCategoria(spinerCategorias.getSelectedItem().toString());
+                   String textoDescricao = descricao.getText().toString();
+                   String textoValor = valor.getText().toString();
+                   String textoData =  data.getText().toString();
+                  // String textoCategoria = spinerCategorias.getItemAtPosition(onItemSelected(position)).toString();
 
-                Toast.makeText(InserirGastosPropiosActivity.this,"Inserido com sucesso",Toast.LENGTH_LONG).show();
-//                reference.child(String.valueOf(maxid + 1)).setValue(gastosPropios);
+                if (textoDescricao.isEmpty()){
+                       Toast.makeText(InserirGastosPropiosActivity.this,"Digite a descrição!", Toast.LENGTH_LONG).show();
+
+                   } else {
+                                 GastosPropios gastosPropios = new GastosPropios();
+                                   gastosPropios .setIdUtilizador(idUtilizadorGP);
+                                   gastosPropios .setDecricao(textoDescricao);
+                                   gastosPropios .setData(textoData);
+                                   gastosPropios .setValor(textoValor);
+                                  // gastosPropios  .setSpinnerCategoria(textoCategoria);
+
+                                   salvarGastosProprios(idUtilizadorGP,gastosPropios);
+                                   descricao.setText("");
+                                   valor.setText("");
+                                   data.setText("");
+                   }
+
             }
         });
-    }
-    /*
 
-    private ArrayList<CustomItem> getCustomList() {
-        customList = new ArrayList<>();
-        customList.add(new CustomItem("Alimentação",R.drawable.ic_alimentacao_24dp));
-        customList.add(new CustomItem("Bares e Restaurantes",R.drawable.ic_bar_restaurante_24dp));
-        customList.add(new CustomItem("Casa",R.drawable.ic_casa));
-        customList.add(new CustomItem("Compras",R.drawable.ic_compras));
-        customList.add(new CustomItem("Cuidados Pessoais",R.drawable.ic_cuidados_pessoais_24dp));
-        customList.add(new CustomItem("Educação",R.drawable.ic_educacao_24));
-        customList.add(new CustomItem("Família e Filhos",R.drawable.ic_familia_filhos));
-        customList.add(new CustomItem("Lazer e hobbies",R.drawable.ic_lazer_hobbies));
-        customList.add(new CustomItem("Mercado",R.drawable.ic_mercado));
-        customList.add(new CustomItem("Presentes",R.drawable.ic_presentes_24dp));
-        customList.add(new CustomItem("Pets",R.drawable.ic_pets));
-        customList.add(new CustomItem("Transporte",R.drawable.ic_transporte));
-        customList.add(new CustomItem("Trabalho",R.drawable.ic_trabalho2));
-        customList.add(new CustomItem("Seguro",R.drawable.ic_seguro_24dp));
-        customList.add(new CustomItem("Viagem",R.drawable.ic_viagem));
-        return customList;
-    }
-    */
 
+    }
+
+    public boolean salvarGastosProprios(String idUtilizadorGP, GastosPropios gastosPropios){
+        try{
+            firebase = ConfiguracaoFirebase.getFirebase().child("Gastos Próprios");
+            firebase.child(idUtilizadorGP)
+                    .push()
+                    .setValue(gastosPropios);
+
+            return true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -168,9 +176,9 @@ public class InserirGastosPropiosActivity extends AppCompatActivity implements  
 
     }
 
-       public  void abrirGastosPorCategoria(View view){
-           Intent intent = new Intent(InserirGastosPropiosActivity.this,InserirGastosPorCategoriaActivity.class);
-           startActivity(intent);
-           finish();
-       }
+    public  void abrirGastosPorCategoria(View view){
+        Intent intent = new Intent(InserirGastosPropiosActivity.this,InserirGastosPorCategoriaActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
